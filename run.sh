054405d6a4d4c1da3a91ea62ec6eb3a7c55c08b5
zhouyq <zyq916@gmail.com>
2016-08-02 12:54:52 +0800

    修改持久化目录，增加配置文件处理功能

--- run.sh
+++ run.sh
@@ -2,38 +2,21 @@
 
 [ $DEBUG ] && set -x
 
-Dirs="system tmp cache uploads"
-PermanentDir="/data"
 AppDir="/app"
 UserCfg="${AppDir}/system/config/database.php"
 InstallFile="${AppDir}/install/index.php"
+TmpCfgDir=/app/.config
 
-
-# 在持久化存储中创建需要的目录
-for d in $Dirs
-do
-  # 检测是否包含子目录
-  subdir=`dirname $d`
-
-  if [ ! -d ${PermanentDir}/${d} ] ;then
-  
-    if [ "$subdir" == "." ];then
-      [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/${d} || mkdir -pv ${PermanentDir}/${d}
-    else
-      [ ! -d ${PermanentDir}/$subdir ] && mkdir -pv ${PermanentDir}/$subdir
-      [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${PermanentDir}/$subdir
-    fi
-  else
-    [ -d ${AppDir}/${d} ] && mv ${AppDir}/${d} ${AppDir}/${d}.bak
-  fi
-  ln -s ${PermanentDir}/${d} ${AppDir}/${d}
-done
+# 如果已经保存了数据库配置文件，做软连接处理
+[ -f $TmpCfgDir/database.php ] && ln -s $TmpCfgDir/database.php $UserCfg
 
 # 如果存在my.php 清理install.php和upgrade.php 文件
 if [ -f $UserCfg ];then
   [ -f $InstallFile ] && rm -f $InstallFile
 fi
 
+# crontab for process config file
+crontab cron.txt && cron
 
 # 启动web server
 vendor/bin/heroku-php-nginx -F fpm_custom.conf
